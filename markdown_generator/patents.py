133f3db38d2c4e93b3db24a80c1c98a798163de9
AGyorgypal <105825224+AGyorgypal@users.noreply.github.com>
2024-12-28 12:49:24 -0500

    Update patents.py

--- markdown_generator/patents.py
+++ markdown_generator/patents.py
@@ -93,7 +93,7 @@ for row, item in patents.iterrows():
     ## Markdown description for individual page
     
     if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download patent here</a>\n" 
+        md += "\n\n<a href='" + item.paper_url + "'>Download patent</a>\n" 
         
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
